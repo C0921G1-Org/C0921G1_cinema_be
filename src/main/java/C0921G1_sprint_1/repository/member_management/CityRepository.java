21ffa5b4fb364dcf365605c25fe9ffbc592e0f29
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2022-03-25 12:46:45 +0700

    khanh update database

--- src/main/java/C0921G1_sprint_1/repository/member_management/CityRepository.java
+++ src/main/java/C0921G1_sprint_1/repository/member_management/CityRepository.java
@@ -1,9 +1,9 @@
 package C0921G1_sprint_1.repository.member_management;
 
-import C0921G1_sprint_1.model.member.Member;
+import C0921G1_sprint_1.model.member.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member,String> {
+public interface CityRepository extends JpaRepository<City,Integer> {
 }
