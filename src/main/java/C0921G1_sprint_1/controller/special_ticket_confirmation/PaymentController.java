e6b3d34c41347d7241e780d87a3b991311cb6104
Doantaihuynh100983 <doantai100983hu@gmail.com>
2022-03-25 13:47:58 +0700

    tai pull

--- src/main/java/C0921G1_sprint_1/controller/special_ticket_confirmation/PaymentController.java
+++ src/main/java/C0921G1_sprint_1/controller/special_ticket_confirmation/PaymentController.java
@@ -1,4 +1,26 @@
 package C0921G1_sprint_1.controller.special_ticket_confirmation;
 
+
+import C0921G1_sprint_1.model.transaction.Transaction;
+import C0921G1_sprint_1.service.special_ticket_confirmation.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/pay")
+    public ResponseEntity<List<Transaction>> listResponseEntity(){
+        List<Transaction> transactions = paymentService.getAllTransaction();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
 }
--- src/main/java/C0921G1_sprint_1/service/special_ticket_confirmation/PaymentServiceImpl.java
+++ src/main/java/C0921G1_sprint_1/service/special_ticket_confirmation/PaymentServiceImpl.java
@@ -1,5 +1,18 @@
 package C0921G1_sprint_1.service.special_ticket_confirmation;
 
+import C0921G1_sprint_1.model.transaction.Transaction;
+import C0921G1_sprint_1.repository.special_ticket_confirmation.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class PaymentServiceImpl {
+import java.util.List;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+    @Autowired
+    private PaymentRepository paymentRepository;
+    @Override
+    public List<Transaction> getAllTransaction() {
+        return paymentRepository.findAll();
+    }
 }
