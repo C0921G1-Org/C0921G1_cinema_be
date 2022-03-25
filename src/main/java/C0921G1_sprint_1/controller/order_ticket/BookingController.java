9d6967e8b58f4d9749c7612a321460dc3427da93
nhatanh203 <vonhatanh462003@gmail.com>
2022-03-25 14:38:30 +0700

    AnhVN push

--- src/main/java/C0921G1_sprint_1/controller/order_ticket/BookingController.java
+++ src/main/java/C0921G1_sprint_1/controller/order_ticket/BookingController.java
@@ -1,4 +1,10 @@
 package C0921G1_sprint_1.controller.order_ticket;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
 public class BookingController {
+
 }
