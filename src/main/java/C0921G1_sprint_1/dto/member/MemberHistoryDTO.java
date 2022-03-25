3c99101404d3e982ff00a709592ff93d1fc5aadc
ChristDrumb0 <nhantn996@gmail.com>
2022-03-25 13:41:59 +0700

    NhanNT

---
+++ src/main/java/C0921G1_sprint_1/dto/member/MemberHistoryDTO.java
@@ -0,0 +1,5 @@
+package C0921G1_sprint_1.dto.member;
+
+//NhanNT create for trading history screen
+public class MemberHistoryDTO {
+}
--- src/main/java/C0921G1_sprint_1/repository/member_account/MemberAccountRepository.java
+++ src/main/java/C0921G1_sprint_1/repository/member_account/MemberAccountRepository.java
@@ -1,4 +1,27 @@
 package C0921G1_sprint_1.repository.member_account;
 
-public interface MemberAccountRepository {
+import C0921G1_sprint_1.dto.member.MemberDTO;
+import C0921G1_sprint_1.model.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberAccountRepository extends JpaRepository<Member,String> {
+    @Query(value =
+            "Select t.transactional_date, t.code, s.name, f.name, se.transaction_id, st.price, ats.price, m.point \n" +
+            " From transaction t \n" +
+            "Join show_time st on t.show_time_id = st.id \n"+
+            "Join film f on st.film_id = f.id \n"+
+            "Join screen s on s.show_time_id = st.id \n"+
+            "Join member m on m.id = t.member_id \n"+
+            "Join seat se on se.transaction_id = t.id \n"+
+            "Join seat_type set on se.seat_type_id = set.id \n"+
+            "Join attached_service ats on ats.transaction_id = t.id \n"+
+            "where m.id = ?1;"
+
+            , nativeQuery = true)
+    Page<MemberDTO> findAll(String name, Integer customerTypeId, Pageable pageable);
 }
