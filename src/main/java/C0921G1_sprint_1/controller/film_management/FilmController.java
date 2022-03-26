c3d2afae27d57cebbd3a64866f7726827ec1694e
manhhung1509 <hung2332nguyenmanh@gmail.com>
2022-03-26 14:32:50 +0700

    hung push

--- src/main/java/C0921G1_sprint_1/controller/film_management/FilmController.java
+++ src/main/java/C0921G1_sprint_1/controller/film_management/FilmController.java
@@ -1,5 +1,49 @@
 package C0921G1_sprint_1.controller.film_management;
 
+import C0921G1_sprint_1.model.film.Film;
+import C0921G1_sprint_1.service.film_management.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/film/")
+
 public class FilmController {
 
+    @Autowired
+    FilmService filmService;
+
+    //Tai DHN Xem Chi Tiết Phim
+    @GetMapping("findById/{id}")
+    public ResponseEntity<Film> findByIdFilm(@PathVariable Integer id){
+        Optional<Film> optionalFilm =filmService.findById(id);
+        if (optionalFilm.isPresent()){
+            return new ResponseEntity<>(optionalFilm.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // HungNM lấy danh sách phim và tìm kiếm phim ở màn hình trang chủ
+    @GetMapping("list-client")
+    public ResponseEntity<Page<Film>> findAllFilmClient(@RequestParam(defaultValue = "0") int page,
+                                                        @RequestParam(defaultValue = "") String startDate,
+                                                        @RequestParam(defaultValue = "") String name,
+                                                        @RequestParam(defaultValue = "") String statusFilm,
+                                                        @RequestParam(defaultValue = "") String typeFilm) {
+        Pageable pageable = PageRequest.of(page, 6);
+        Page<Film> filmPage = filmService.findAllFilmClient(startDate, name, statusFilm, typeFilm, pageable);
+        if (filmPage.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(filmPage, HttpStatus.OK);
+        }
+    }
 }
--- src/main/java/C0921G1_sprint_1/service/film_management/FilmServiceImpl.java
+++ src/main/java/C0921G1_sprint_1/service/film_management/FilmServiceImpl.java
@@ -1,5 +1,33 @@
 package C0921G1_sprint_1.service.film_management;
 
-public class FilmServiceImpl implements FilmService{
 
+import C0921G1_sprint_1.model.film.Film;
+import C0921G1_sprint_1.repository.film_management.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+
+@Service
+public class FilmServiceImpl implements FilmService {
+    @Autowired
+    private FilmRepository filmRepository;
+
+    //Tai DHN Xem Chi Tiết Phim
+    @Override
+    public Optional<Film> findById(Integer id) {
+        return filmRepository.findById(id);
+    }
+
+
+    // HungNM lấy danh sách phim và tìm kiếm phim ở màn hình trang chủ
+    @Override
+    public Page<Film> findAllFilmClient(String startDate, String name, String statusFilm, String typeFilm, Pageable pageable) {
+        return filmRepository.findAllFilmClient(startDate, name, statusFilm, typeFilm, pageable);
+    }
 }
