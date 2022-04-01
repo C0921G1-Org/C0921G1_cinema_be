5d4e8b0785eee96f745dc46c6b5c422c14124d1a
manhhung1509 <hung2332nguyenmanh@gmail.com>
2022-04-01 18:40:39 +0700

    Tai Pull Dev

--- src/main/java/C0921G1_sprint_1/security/config/WebSecurityConfig.java
+++ src/main/java/C0921G1_sprint_1/security/config/WebSecurityConfig.java
@@ -1,60 +1,60 @@
-package C0921G1_sprint_1.security.config;
-
-import C0921G1_sprint_1.security.jwt.JwtTokenFilter;
-import C0921G1_sprint_1.security.services.UserDetailServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-/**
- * TuNk
- */
-
-@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailServiceImpl userDetailService;
-    @Autowired
-    private JwtTokenFilter jwtTokenFilter;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService);
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                .antMatchers("/c09/public/**")
-                .permitAll()
-                .antMatchers("/c09/user/**")
-                .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/c09/admin/**")
-                .hasRole("ADMIN")
-                .anyRequest().authenticated();
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
-}
+//package C0921G1_sprint_1.security.config;
+//
+//import C0921G1_sprint_1.security.jwt.JwtTokenFilter;
+//import C0921G1_sprint_1.security.services.UserDetailServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+///**
+// * TuNk
+// */
+//
+//@Configuration
+////@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private UserDetailServiceImpl userDetailService;
+//    @Autowired
+//    private JwtTokenFilter jwtTokenFilter;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailService);
+//    }
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .authorizeRequests()
+//                .antMatchers("/c09/public/**")
+//                .permitAll()
+//                .antMatchers("/c09/user/**")
+//                .hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/c09/admin/**")
+//                .hasRole("ADMIN")
+//                .anyRequest().authenticated();
+//        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//}
